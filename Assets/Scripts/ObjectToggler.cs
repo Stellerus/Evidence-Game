81f5cbc97deae6c0351028af4dce99d971f66063
LeonidMotolko <147403563+LeonidMotolko@users.noreply.github.com>
2025-06-04 11:49:06 +0300

    Translate comments to Eng

--- Assets/Scripts/ObjectToggler.cs
+++ Assets/Scripts/ObjectToggler.cs
@@ -2,35 +2,35 @@ using UnityEngine;
 
 public class ObjectToggler : MonoBehaviour
 {
-    // Массив объектов, которые будут включаться/выключаться
+        // Array of objects that will be enabled/disabled
     public GameObject[] objectsToToggle;
     public GameObject ParentObjectToToggle;
 
-    // Этот метод вызывается, когда пользователь кликает мышью по Collider'у этого GameObject'а
+    // This method is called when the user clicks the Collider of this GameObject with the mouse
     void OnMouseDown()
     {
-        // Включаем каждый объект из массива
+        // Enable each object in the array
         foreach (GameObject obj in objectsToToggle)
         {
-            if (obj != null) // Проверяем, что объект назначен в массиве
+            if (obj != null) // Check that the object is assigned in the array
             {
-                obj.SetActive(true); // Делаем объект активным
+                obj.SetActive(true); // Activate the object
             }
         }
     }
 
-    // Метод Update вызывается каждый кадр
+    // The Update method is called every frame
     void Update()
     {
-        // Проверяем, нажата ли клавиша Escape
+        // Check if the Escape key is pressed
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            // Выключаем каждый объект из массива
+            // Disable each object in the array
             foreach (GameObject obj in objectsToToggle)
             {
-                if (obj != null) // Проверяем, что объект назначен в массиве
+                if (obj != null) // Check that the object is assigned in the array
                 {
-                    obj.SetActive(false); // Делаем объект неактивным
+                    obj.SetActive(false); // Deactivate the object
                 }
             }
         }
