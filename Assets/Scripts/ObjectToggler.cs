15f09e4aa7cb7300b75a89d84f3e02b6b279f823
Vladislav Stepanenko <fedkinapochta@gmail.com>
2025-06-04 21:28:21 +0300

    gol

--- Assets/Scripts/ObjectToggler.cs
+++ Assets/Scripts/ObjectToggler.cs
@@ -5,10 +5,12 @@ public class ObjectToggler : MonoBehaviour
         // Array of objects that will be enabled/disabled
     public GameObject[] objectsToToggle;
     public GameObject ParentObjectToToggle;
+    public ScreenFader fader; // Reference to the ScreenFader script
 
     // This method is called when the user clicks the Collider of this GameObject with the mouse
     void OnMouseDown()
     {
+        fader.Enable(); // Call the Enable method on the ScreenFader script
         // Enable each object in the array
         foreach (GameObject obj in objectsToToggle)
         {
