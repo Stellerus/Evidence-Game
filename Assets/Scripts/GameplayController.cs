3741f7c452c2cc5d56c5e38cb5bae90882877928
Vladislav Stepanenko <fedkinapochta@gmail.com>
2025-06-04 16:23:48 +0300

    переработал слегка диалоги, добавил эфект пишушей машинки

--- Assets/Scripts/GameplayController.cs
+++ Assets/Scripts/GameplayController.cs
@@ -1,138 +1,113 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogueSwitchS : MonoBehaviour
 {
-    public GameObject[] dialogueTexts; // Массив объектов текста
-    public GameObject targetObject0;
-    public GameObject targetObject1;
-    public GameObject targetObject2;
-    public GameObject targetObject3;
-    public GameObject Character1;
-    public GameObject Character2;
-    public GameObject Character3;
+    [Header("Text Objects")]
+    [SerializeField] private GameObject[] dialogueTexts;
+    [SerializeField] private float startDelay = 0.5f;
 
-    private int currentIndex = 0;
+    [Header("Scene Objects")]
+    [SerializeField] private GameObject[] targetObjects;
+    [SerializeField] private GameObject[] characters;
 
-    public PhoneCallingScript phoneCallingScript;
+    [Header("Dependencies")]
+    [SerializeField] private PhoneCallingScript phoneCallingScript;
+
+    private int currentIndex = -1;
+    private Typewriter3DEffect currentTypewriter;
+
+    void Start()
+    {
+        InitializeTextObjects();
+    }
+
+    private void InitializeTextObjects()
+    {
+        foreach (var textObj in dialogueTexts)
+        {
+            if (textObj != null)
+            {
+                textObj.SetActive(false);
+                var tw = textObj.GetComponent<Typewriter3DEffect>();
+                if (tw != null) tw.enabled = true;
+            }
+        }
+    }
+
     public void NextDialogue()
     {
-        // Выключаем текущий текст, если он есть
-        if (currentIndex < dialogueTexts.Length)
-            dialogueTexts[currentIndex].SetActive(false);
+        // Отключаем предыдущий текст
+        if (currentIndex >= 0 && currentIndex < dialogueTexts.Length)
+        {
+            if (dialogueTexts[currentIndex] != null)
+            {
+                dialogueTexts[currentIndex].SetActive(false);
+                if (currentTypewriter != null)
+                    currentTypewriter.SkipTyping();
+            }
+        }
 
         currentIndex++;
 
-        switch (currentIndex)
+        if (HandleSpecialCases(currentIndex))
+            return; // Если был особый случай — выход, не активируем текст
+
+        // Активация нового текста
+        if (currentIndex < dialogueTexts.Length && dialogueTexts[currentIndex] != null)
         {
-            case 1:
-                dialogueTexts[1].SetActive(true);
-                break;
-            case 2:
-                dialogueTexts[2].SetActive(true);
-                break;
-            case 3:
-                dialogueTexts[3].SetActive(true);
-                break;
-            case 4:
-                dialogueTexts[4].SetActive(true);
-                break;
-            case 5:
-                dialogueTexts[5].SetActive(true);
-                break;
-            case 6:
-                dialogueTexts[6].SetActive(true);
-                break;
-            case 7:
-                dialogueTexts[7].SetActive(true);
-                break;
-                case 8:
-                    dialogueTexts[8].SetActive(true);
-                break;
-                case 9:
-                phoneCallingScript.StartCoroutine(phoneCallingScript.Stage2Delay());
-                gameObject.SetActive(false);
-                targetObject0.SetActive(false);
-                Character1.SetActive(false);
-                break;
-                case 10:
-                dialogueTexts[10].SetActive(true);
-                break;
-                case 11:
-                    dialogueTexts[11].SetActive(true);
-                break;
-                case 12:
-                gameObject.SetActive(false);
-                targetObject1.SetActive(false);
-                break;
-                case 13:
-                    dialogueTexts[13].SetActive(true);
-                break;
-                case 14:
-                    dialogueTexts[14].SetActive(true);
-                break;
-                case 15:
-                    dialogueTexts[15].SetActive(true);
-                break;
-                case 16:
-                    dialogueTexts[16].SetActive(true);
-                break;
-                case 17:
-                    dialogueTexts[17].SetActive(true);
-                break;
-                case 18:
-                    dialogueTexts[18].SetActive(true);
-                break;
-                case 19:
-                gameObject.SetActive(false);
-                targetObject2.SetActive(false);
-                Character2.SetActive(false);
-                break;
-                case 20:
-                    dialogueTexts[20].SetActive(true);
-                break;
-                case 21:
-                    dialogueTexts[21].SetActive(true);
-                break;
-                case 22:
-                    dialogueTexts[22].SetActive(true);
-                break;
-                case 23:
-                    dialogueTexts[23].SetActive(true);
-                break;
-                case 24:
-                    dialogueTexts[24].SetActive(true);
-                break;
-                case 25:
-                    dialogueTexts[25].SetActive(true);
-                break;
-                case 26:
-                    dialogueTexts[26].SetActive(true);
-                break;
-                case 27:
-                    dialogueTexts[27].SetActive(true);
-                break;
-                case 28:
-                    dialogueTexts[28].SetActive(true);
-                break;
-                case 29:
-                    dialogueTexts[29].SetActive(true);
-                break;
-                case 30:
-                    dialogueTexts[30].SetActive(true);
-                break;
-            case 31:
-                dialogueTexts[31].SetActive(true);
-                break;
-                case 32:
-                    dialogueTexts[32].SetActive(true);
-                break;
-            default:
-                gameObject.SetActive(false);
-                targetObject3.SetActive(false);
-                Character3.SetActive(false);
-                break;
+            dialogueTexts[currentIndex].SetActive(true);
+            currentTypewriter = dialogueTexts[currentIndex].GetComponent<Typewriter3DEffect>();
+            if (currentTypewriter != null)
+                currentTypewriter.StartTyping();
         }
     }
+
+    private bool HandleSpecialCases(int index)
+    {
+        switch (index)
+        {
+            case 9:
+                phoneCallingScript.StartCoroutine(phoneCallingScript.Stage2Delay());
+                ToggleObjects(false, 0);
+                ToggleCharacter(false, 0);
+                return true;
+
+            case 12:
+                ToggleObjects(false, 1);
+                return true;
+
+            case 19:
+                ToggleObjects(false, 2);
+                ToggleCharacter(false, 1);
+                return true;
+
+            case 32:
+                ToggleObjects(false, 3);
+                ToggleCharacter(false, 2);
+                return true;
+
+            default:
+                if (index >= dialogueTexts.Length)
+                {
+                    gameObject.SetActive(false);
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    private void ToggleObjects(bool state, int targetIndex)
+    {
+        if (targetIndex >= 0 && targetIndex < targetObjects.Length)
+            targetObjects[targetIndex].SetActive(state);
+    }
+
+    private void ToggleCharacter(bool state, int characterIndex)
+    {
+        if (characterIndex >= 0 && characterIndex < characters.Length)
+            characters[characterIndex].SetActive(state);
+    }
 }
+
 
---
+++ Assets/Scripts/TypewriterEffect.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(TextMesh))]
+public class Typewriter3DEffect : MonoBehaviour
+{
+    [SerializeField] private float charsPerSecond = 20;
+    [SerializeField] private float punctuationPause = 0.5f;
+
+    private TextMesh textMesh;
+    private string fullText;
+    private Coroutine typingCoroutine;
+    private bool isSkipping;
+
+    void Awake()
+    {
+        textMesh = GetComponent<TextMesh>();
+        fullText = textMesh.text;
+        textMesh.text = "";
+    }
+
+    public void StartTyping()
+    {
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
+
+        typingCoroutine = StartCoroutine(TypeCharacters());
+    }
+
+    private IEnumerator TypeCharacters()
+    {
+        textMesh.text = "";
+        int currentChar = 0;
+
+        while (currentChar < fullText.Length)
+        {
+            if (isSkipping)
+            {
+                textMesh.text = fullText;
+                yield break;
+            }
+
+            textMesh.text += fullText[currentChar];
+
+            // Пауза после знаков препинания
+            if (IsPunctuation(fullText[currentChar]))
+                yield return new WaitForSeconds(punctuationPause);
+            else
+                yield return new WaitForSeconds(1 / charsPerSecond);
+
+            currentChar++;
+        }
+    }
+
+    public void SkipTyping()
+    {
+        isSkipping = true;
+    }
+
+    private bool IsPunctuation(char c)
+    {
+        return c == '.' || c == '!' || c == '?' || c == ';' || c == ':';
+    }
+}
+
