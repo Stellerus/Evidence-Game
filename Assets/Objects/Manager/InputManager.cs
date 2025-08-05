7aa51707582ba017950ca77700a3c2e88418fcb7
seachran <motolko.leonid.1980@gmail.com>
2025-08-06 01:51:06 +0300

    update InputManager script; add KeyBinds_SO script

--- Assets/Objects/Manager/InputManager.cs
+++ Assets/Objects/Manager/InputManager.cs
@@ -3,17 +3,5 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    [Header("List of keys")]
-    public List<KeyCode> usedKeyCodes = new List<KeyCode>();
-
-    private void Reset()
-    {
-        // Fill in only English letters A-Z
-        usedKeyCodes.Clear();
-        for (char c = 'a'; c <= 'z'; c++)
-        {
-            KeyCode key = (KeyCode)System.Enum.Parse(typeof(KeyCode), c.ToString().ToUpper());
-            usedKeyCodes.Add(key);
-        }
-    }
+    public KeyBinds_SO KeyBinds;
 }
