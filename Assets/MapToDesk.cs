efcd38e7f813054ba689a57e06dc2acf57a8a51b
Stellerus <77343982+Stellerus@users.noreply.github.com>
2025-11-05 17:53:21 +0200

    counter returns the movie

--- Assets/MapToDesk.cs
+++ Assets/MapToDesk.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class MapToDesk : MonoBehaviour
 {
 
-    MapBehaviour mapBehaviour;
-    // Insert Desk Folder Script
+    public MapBehaviour mapBehaviour;
+    public FolderButton2D tableBehaviour;
 
+    public void GetDocumentsToTable()
+    {
+        GetDocuments(mapBehaviour.GetActiveCrimePoints(), tableBehaviour.sheetPrefabs);
+    }
     public List<GameObject> GetDocuments(List<CrimePointBehaviour> crimePoints, List<GameObject> documents)
     {
 
         foreach (CrimePointBehaviour point in crimePoints)
         {
-            //if (point@@ is not null && !documents.Contains(point))
-            //{
-            //    documents.Add(point@@);
-            //}
+            if (point is not null && !documents.Contains(point.gameObject))
+            {
+                documents.Add(point.gameObject);
+            }
 
         }
 
